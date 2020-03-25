a785da5c306472d6c7509e761a41cf96206a9ccb
John <jae5142@gmail.com>
2020-03-25 14:33:40 -0400

    1st Commit

---
+++ twitterScraper/twitterKeys.py
@@ -0,0 +1,4 @@
+consumer_key = "OW6SW0aGbtpih4obt6gVj8H7P"
+consumer_secret = "LWncd6rgPLRXN9FyooJNZg7cu43fNtdV9EH8fYdJpkhV88aHuR"
+access_token = "1176324235008061442-1qfz75qktR2oP6DKLxSwYHZa6ZFikq"
+access_token_secret = "48L0Wuey2mfZVln1jnpwVfevrWMtrQN2nkdf4zKfx4uek"
